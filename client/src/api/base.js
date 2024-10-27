abf6ca7c981a3f09818a2bbd5f1cfde39bf7aa96
Aryan Panchal <panchalaryan698@gmail.com>
2024-10-27 18:23:40 +0530

    frontend : backend link update

--- client/src/api/base.js
+++ client/src/api/base.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_BASE_URL = "http://localhost:8000/api"
+const SERVER_BASE_URL = "https://event-calender-pbwx.onrender.com/api"
 
 const api = axios.create({
     headers: {
