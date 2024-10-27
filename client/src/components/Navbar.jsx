92ad3365bfacbe8a141963474147974b4e471468
Aryan Panchal <panchalaryan698@gmail.com>
2024-10-27 20:48:27 +0530

    frontend : user avatar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -131,7 +131,7 @@ function ResponsiveAppBar() {
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Open settings">
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, m: 0 }}>
-                                <Avatar alt="User Avatar" src="/static/images/avatar/2.jpg" />
+                                <Avatar alt={user ? user.username : "A"} src="/static/images/avatar/2.jpg" />
                             </IconButton>
                         </Tooltip>
                         <Menu
